d9f058aeb166baeef9e1880ed50b98496e99859a
SimranKausal <161968773+SimranKausal@users.noreply.github.com>
2024-03-15 17:28:44 +0000

    changes

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,11 @@
-console.log("Simran")
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId= 2
+console.log(accountId)
+accountEmail  = "hc@hc.com"
+accountPassword  = "212121"
+accountCity = "Bangalore"
+console.table([accountEmail, accountPassword, accountCity])
