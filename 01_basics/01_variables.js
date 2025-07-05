1be8c5ed1e32fca31caaa73037e5855fad22c8a0
Simran Kaushal <161968773+SimranKausal@users.noreply.github.com>
2025-07-05 16:57:06 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+//prefer not to use var because of its scoping na dfunctioning earlier version of javavscript did not have scopes functional so there was problem that is why we dont use var now 
+
+
+
+const accountId = 144553;
+let accountEmail = "kaushalsimran35@gmail.com";
+var accountPassword = "Simran Kaushal";
+
+
+console.table([accountId,accountEmail,accountPassword]);
+
+
+
+fullname = "Simran kaushal";
+// in javvscript there is a functionality that without writing let var or const we can just write the variabke and assign it
+console.log(fullname);
+
+
+let myPassword;
+console.log(myPassword);
+// if we left the declared variable without initialising it then its value will be undefined
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict" //(treat all code as new version of javascript)
+
+
+alert()//we cannot use alert here a we are in the node
+
+
+
+//data types in js
+fullname = "simran "// in javavscript we almost write code without semicolon in the end
+age = 23
+isloggedin = 'true'
+
+
+//Datatypes in js
+
+//number = ranges for the number to store 2 to the power 53
+//BigInt = for storing very large numbers
+//string = always written inside doublw quotes
+//boolean = for yes or no (true and false)
+//null = standalone value // representation of an empty value (intentionally declaring the value as null)
+//undefined = value not yet defined
+//symbol= used for uniqueness
+//objects 
+//typeof(null) is object and typeof(undefined)is undefied itself
+
+
+
+
