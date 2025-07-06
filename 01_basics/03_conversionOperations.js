4558b209dbcbeed4e8865daf45179d9a2f76ce10
Simran Kaushal <161968773+SimranKausal@users.noreply.github.com>
2025-07-06 03:02:49 +0000

    conversionoperations done

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,44 @@
+// let score = 33;//this we know its data type is number
+// console.log(typeof (score)) //here we print its datatype
+
+
+// let score = "33";
+// console.log(typeof(score))
+// let scorenumber = Number(score)
+// console.log(scorenumber)// here we have chnaged the dataype of the string to number
+
+
+// let score = "33abc"//suppose we have a score = 3abc now we need to convert it in number then ....
+// console.log(typeof(score))
+// let valueinNumber = Number(score)
+// console.log(typeof(valueinNumber))
+// console.log(valueinNumber)
+
+
+// let score = null //so converting null to number gives us zero
+// let nulltoNumber = Number(score)
+// console.log(nulltoNumber)
+
+// let score = undefined
+// console.log(Number(score))//converting na undefined value into number gives use Nan(Not a number)
+
+
+
+// let isloggedin = true//this is the boolean value and we need to convert it into number it gives us 1
+// console.log(Number(isloggedin))
+//and if the vALUE GIVEN AS FALSE THE NUMBER CONVERSION WILL GIVE US ZERO
+
+
+// lets now convert a 1 value into boolean 
+// let isloggedin = 1;
+// console.log(Boolean(isloggedin))//we will  have a value as true   
+
+
+//converting an empty string into number gives us 0 and converting into boolean gives us false and if we give non empty string it will gives us 1 and true simultaneously
+// let score = ""
+// console.log(Boolean(score))
+
+
+
+
+
