4ba811226d93600da52ce0017e40c4fe7e59c40b
Simran Kaushal <161968773+SimranKausal@users.noreply.github.com>
2025-07-09 17:27:02 +0000

    functions

---
+++ 03_basics.js/01_functions.js
@@ -0,0 +1,12 @@
+function SayMyname() {//...//function definition
+    console.log("S")
+    console.log("I")
+    console.log("M")
+    console.log("R")
+    console.log("A")
+    console.log("N")
+
+}
+
+//SayMyname//...is reference
+SayMyname();//this is execution
