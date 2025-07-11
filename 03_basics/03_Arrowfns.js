3f65d22887af31ac7ee40e5a4b8c5ba1c14050df
Simran Kaushal <161968773+SimranKausal@users.noreply.github.com>
2025-07-11 18:01:03 +0000

    arrowfns

--- 03_basics/03_Arrowfns.js
+++ 03_basics/03_Arrowfns.js
@@ -40,3 +40,34 @@ return a+b;// explicit return ...matlab explicitly lagana pdh raha hai normal
    // suppose if we want to return the object in implicit return then also we need to wrap the object in the () brackets
    // example - const multiply = (m,n) => ({...object...})
 
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++IIFE(Immediately Invokeed Function Expression)++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//sometimes we dont want our function variables  to  get polluted by our global declared variables so for that we use immediately invoked function to execute immidiately upon its declaration so for these reasons we use IIFE
+ // so this is the classical way of writing function (function chai() {
+//                                                   console.log("Database Connected")
+//                                                                   })();// function chai is wrapped under for securing it from pollution or global variable and the next set of bracket are functions normal brackets nad terminate semicolon is to terminate or to stop the fn because this funcyion was immediately invoked so that is why to stop it
+
+
+// doing iife with another function syntax
+(function () {
+  console.log("database connected 2");
+})();
+
+
+// and the third way of writing iife functions with arrow function syntax
+(() =>{
+  console.log("DB Connected 3");
+})();
+
+//how to give arguments to an IIFE FUNCTION?
+  // suppose we want to make an iife fn which prints name 
+
+  // ((name)=>{
+  //   console.log(` DB connected Mr.${name}`);
+  // })('hitesh');
+
+
+  ((name) => {
+  console.log(`DB connected Mr.${name}`);
+})('hitesh'); // so thi is how we nned to pass arguments to iife function and we need to end every statement in the iife function with semicolon its compulsory itherwise we will have to handle errors
+
+/// the iife which has name is known as named iffe and which do not containes a name known as un named which and similarly those with parameters and non parameters simulataneously 
