49825e7eb9778112939df54f4a1b86e9bffa6b44
Simran Kaushal <161968773+SimranKausal@users.noreply.github.com>
2025-07-14 10:13:51 +0000

    maps

--- 05_iterators/maps.js
+++ 05_iterators/maps.js
@@ -1,5 +1,11 @@
- let map =  map.set('in','india');
-            map.set('usa','United states of america')
-            map.set('fr','France')
-            
-console.log(map);
+ let map =  new Map();
+ map.set("in","india")
+ map.set("USA","united states of America")
+ map.set('fr','France')
+
+ console.log(map);
+  // maps are just like objects but they do not contain repeated values , they always conatin unique values and they remember the order in which thw data was entered into them
+
+for (const key of map) {
+    console.log(key);
+}
